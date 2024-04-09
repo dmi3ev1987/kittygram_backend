589dc72da86a6b8d4c701d6d25aeb7d676a02500
Андрей Дмитриев <andrey.dmi3ev@gmail.com>
2024-04-09 18:47:33 +0300

    Connect frontend & backend.

--- kittygram_backend/settings.py
+++ kittygram_backend/settings.py
@@ -19,12 +19,14 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'djoser',
+    'corsheaders',
     'cats.apps.CatsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -110,3 +112,9 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+CORS_URLS_REGEX = r'^/api/.*$'
